35bc71af69d84d1a331cc2bdfcd6700743faa709
dsaulny <dsauln1@lsu.edu>
2023-08-25 10:30:17 -0500

    Update to CLI and Input
    + catches invalid inputs and re-prompts user
    + clears terminal when starting game

--- Guess_The_Number__CLI.py
+++ Guess_The_Number__CLI.py
@@ -10,39 +10,63 @@ Description:
     If the user does not guess the number by their 5th turn, the user loses
 """
 
-
+import os
 import random
+
 def guessTheNumber():
-    #intro
-    print("Welcome to Guess The Number!!!\n\n" +
+    
+    # clean terminal
+    os.system("cls") if os.name == "nt" else os.system("clear")
+   
+    # game instructions
+    print("\n\nWelcome to Guess The Number!!!\n\n" +
         "Instructions:\n\t" +
         "You get 5 attempts to guess the number.\n\t" +
         "The number exists in the range 0-100.\n\t" +
         "After each guess, you'll be told whether the number is higher or lower.\n\t" +
         "Let's get started!!!\n")
-    #variables
+    
+    # variables
     number = random.randint(1,99)
-    victory = 0
-    #game
+    victory = False
+    
+    # game
     for _ in range(5):
-        guess = int(input("Enter your guess: "))
+        
+        # user input
+        while True:
+            try:
+                guess = int(input("Enter your guess: "))
+                break
+            except:
+                print("Invalid input, try again.\n")
+    
         if number == guess: 
             print(f"Congratulations!!! You won!!! The number was {guess}.\n")
-            victory = 1
+            victory = True
             break
+
         else:
             number > guess and print(f"The number is higher than {guess}.\n")
             number < guess and print(f"The number is lower than {guess}.\n")
 
-    victory == 0 and print(f"You lose. The number was {number}\n")
+    victory == False and print(f"You lose. The number was {number}\n")
+    
+    # play again
+    while True:
+        p = input("Play again? (Y/N): ")
+        try:
+            if p == 'y':
+                guessTheNumber()
 
-    p = input("Play again? (Y/N): ")
-    if p == 'y':
-        guessTheNumber()
-    elif p =='n':
-        print("Thank you for playing.")
-        exit()
-    elif p not in ('n', 'y'):
-        print("Invalid input!! Exiting game...")
+            elif p =='n':
+                print("Thank you for playing.\n\n")
+                exit()
+
+            elif p not in ('n', 'y'):
+                raise ValueError
+            
+        except ValueError:
+            print("Invalid input, try again.\n")
 
 guessTheNumber()
